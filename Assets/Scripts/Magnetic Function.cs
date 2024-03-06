c09d7609471b07d833c0d828bd925bccc34d3267
Anthony Lee <anthonyghlee@g.ucla.edu>
2024-03-06 04:22:23 -0800

    magnets

--- Assets/Scripts/Magnetic Function.cs
+++ Assets/Scripts/Magnetic Function.cs
@@ -3,49 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class MagneticFunction : MonoBehaviour
 {
-    // Component references
-    // TODO: create variable to store rigidbody of player (2D)
+    
     Rigidbody2D rb;
-    // TODO: create variable storing the Animator
-    Animator anim;
 
-    [SerializeField] float speed = 5f;
-    [SerializeField] float jumpHeight = 5f;
     [SerializeField] float magStrengthVert = 1f;
     [SerializeField] float magStrengthHoriz = 5f;
     [SerializeField] float castDistance = 5f;
 
-    //TODO: keep track of current horizontal movement direction
-    float direction = 0f;
-
-    //keep track of if the player is on the ground
     bool isGrounded = false;
-
-    //TODO: keep track of which direction player is facing
-    bool isFacingRight = true;
-
     bool isRepel = false;
     bool isHitUp = false;
     bool isHitDown = false;
     bool isHitRight = false;
     bool isHitLeft = false;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        // TODO: Get references to the rigidbody and animator attached to the current GameObject
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // TODO: Pass in the proper direction that the player should move in
-        Move(direction);
-
         if (Input.GetKey(KeyCode.UpArrow))
         {
             MagnetUp(); 
@@ -64,17 +46,6 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             MagnetLeft(); 
-        }
-
-        //while((isHitUp == true) || (isHitDown == true) || (isHitRight == true) || (isHitLeft == true))
-        //{
-        //    speed = 0f;
-        //}
-
-        // TODO: check conditions needed to flip player, and if so, flip player
-        if((isFacingRight && direction == -1) || (!isFacingRight && direction == 1))
-        {
-            Flip();
         }
     }
 
@@ -271,46 +242,6 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void OnJump()
-    {
-        //if player is on the ground, jump
-        //if (isGrounded)
-        if (isGrounded)
-        {    
-            Jump();
-        }
-    }
-
-    private void Jump()
-    {
-        // TODO: change y velocity of player
-        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-    }
-
-    void OnMove(InputValue moveVal)
-    {
-        // TODO: store direction input and store it to global variable
-        float movDirection = moveVal.Get<float>();
-        direction = movDirection;
-    }
-
-    private void Move(float x)
-    {
-        // TODO: change x velocity of player
-        rb.velocity = new Vector2(x * speed, rb.velocity.y);
-        // TODO: Here, we can handle animation transitioning logic
-        anim.SetBool("isRunning", x != 0);
-    }
-
-    private void Flip()
-    {
-        // TODO: flip local scale of player and change global variable that stores which direction player is facing
-        isFacingRight = !isFacingRight;
-        Vector3 newlocalScale = transform.localScale;
-        newlocalScale.x *= -1f;
-        transform.localScale = newlocalScale;
-    }
-
     private void OnCollisionExit2D(Collision2D collision)
     {
         isGrounded = false;
@@ -332,24 +263,8 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Collectibles"))
-        {
-            Destroy(other.gameObject);
-        }
-    }
-    // TODO: Week 3's assignment needs a couple of extra functions here...
-
-
-    //public void NextLevel()
-    //{
-        //nextSceneToLoad = SceneManageer.GetActiveScene().buildIndex + 1;
-        //SceneManager.LoadScene(nextSceneToLoad);
-    //}
-
-    // if((isHitUp != true) && (isHitDown != true) && (isHitRight != true) && (isHitLeft != true))
-
-
 }
+
+
+//issues:
+//player still able to run around while the magnet is in place
